a1950e793f80105207868a3d279e943677625cf9
Kayla Reopelle <kreopelle@newrelic.com>
2023-11-28 16:41:05 -0800

    Add pet scaffold

--- examples/newrelicapm/demo/config/initializers/opentelemetry.rb
+++ examples/newrelicapm/demo/config/initializers/opentelemetry.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
+# ENV['OTEL_EXPORTER_OTLP_HEADERS'] = "api-key=#{ENV['NEW_RELIC_API_KEY]}"
+# ENV['OTEL_SERVICE_NAME'] = 'rails71-test'
+# ENV['OTEL_EXPORTER_OTLP_ENDPOINT'] = 'https://otlp.nr-data.net:4317'
+# ENV['OTEL_LOGS_EXPORTER'] = 'otlp'
+
 require 'opentelemetry/sdk'
 require 'opentelemetry/exporter/otlp'
+require 'opentelemetry/exporter/otlp-logs'
 
 OpenTelemetry::SDK.configure(&:use_all)
